e751bcebe41d987f7dfb8bec1cde0e9c5e0dbaea
althearito <altheajazel.rito@lorma.edu>
2024-10-29 00:10:26 +0800

    Update hero page

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -10,7 +10,7 @@ export const Heading = ({
   return (
     <h2
       className={cn(
-        'polygon-path relative z-30 m-auto mb-4 w-max break-words border-[3px] border-green-800/65 px-9 py-3 font-play text-2xl backdrop-blur-[1px] lg:py-5 lg:text-4xl',
+        'polygon-path relative z-30 m-auto mb-4 w-max break-words border-[3px] border-[#030018] px-9 py-3 font-play text-2xl backdrop-blur-[1px] lg:py-5 lg:text-4xl',
         className
       )}
       data-aos="fade"
